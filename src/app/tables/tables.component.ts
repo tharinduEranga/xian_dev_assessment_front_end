a149b68760b2a460d2d50ab10957534c7e769fe2
tharindu-e <tharindue@ceyentra.com>
2020-09-25 23:19:51 +0530

    fixed data table re init when component refreshes

--- src/app/tables/tables.component.ts
+++ src/app/tables/tables.component.ts
@@ -1,6 +1,7 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {CartonService} from '../services/carton.service';
 import Swal from 'sweetalert2';
+import {Subject} from 'rxjs';
 
 declare interface TableData {
     headerRow: string[];
@@ -12,8 +13,13 @@ declare interface TableData {
     templateUrl: './tables.component.html',
     styleUrls: ['./tables.component.css']
 })
-export class TablesComponent implements OnInit {
+export class TablesComponent implements OnDestroy, OnInit {
     public cartonTable: TableData;
+
+    dtOptions: DataTables.Settings = {};
+    // We use this trigger because fetching the list of persons can be quite long,
+    // thus we ensure the data is fetched before rendering
+    dtTrigger: Subject<CartonDTO> = new Subject();
 
     constructor(private cartonService: CartonService) {
     }
@@ -27,6 +33,8 @@ export class TablesComponent implements OnInit {
         // this.isLoading = true;
         this.cartonService.getAll().subscribe(value => {
             this.cartonTable.dataRows = value.content;
+            // Calling the DT trigger to manually render the table
+            this.dtTrigger.next();
         }, error => {
             Swal.fire('Error occurred!', error.message, 'error');
         });
@@ -37,5 +45,15 @@ export class TablesComponent implements OnInit {
             headerRow: ['ID', 'Name', 'Units', 'Price'],
             dataRows: []
         };
+        this.dtOptions = {
+            pagingType: 'full_numbers',
+            pageLength: 10
+        };
     }
+
+    ngOnDestroy(): void {
+        // Do not forget to unsubscribe the event
+        this.dtTrigger.unsubscribe();
+    }
+
 }
